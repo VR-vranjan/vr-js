67b72fa04bdcf614910d1a42ec0a2e009e52a290
VR-vranjan <vranjan00700@gmail.com>
2024-01-15 15:59:32 +0530

    created objects

--- start.js
+++ start.js
@@ -1,2 +1,13 @@
+//print statements
 console.log("hello world");
 console.log("vaishnavi has just started learning js");
+//creating objects
+const student = {
+    name: "vaishnavi ranjan",
+    age: 20,
+    sgpa:9.25,
+    roll: 58
+};
+console.log(student);
+console.log(typeof student.name);
+
